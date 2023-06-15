7f509dc5d9efeb41992fc80aa5cee7be91886f96
adrian.larry <adrian.larry@ui.ac.id>
2023-06-15 22:04:23 +0700

    create API for ML prediction

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
 import uvicorn
 import pickle
+import tensorflow as tf
+from transformers import BertTokenizer
+from fastapi.responses import JSONResponse
+import numpy as np
 
 from pydantic import BaseModel
 
 
 class Comment(BaseModel):
-    comments: list
+    comment: str
 
 app = FastAPI(debug=True)
 
@@ -15,18 +19,45 @@ def home():
     return {"model" : "Sentiment Analysis"}
 
 @app.post('/predict')
-async def predict(listComment: Comment):
-    listComment = listComment.comments
-    print(listComment)
-    # openPickle = open("model/test.pkl", "rb")
-    # model = pickle.load(openPickle)
-    # sum = 0
-    # for comment in listComment:
-    #     makeprediction = model.predict(comment)
-    #     sum += int(makeprediction)
-    # # cari nilai rata2 dari komentar yang di masukkin atau mungkin lu ada cara lain idk
-    # return sum/len(listComment)
-    return "Berhasil jalan"
+async def predict(comment: Comment):
+    # tokenize comments using IndoBERT tokenizer
+    tokens = bert_tokenizer([str(comment)], truncation=True, max_length=100, padding=True, return_tensors="tf")
+    tokenized_input = tokens.input_ids.numpy()
+
+    # prediction
+    input_data = tokenized_input[0]
+    input_data = input_data.reshape(1, -1)
+    if input_data.shape[1] < 100:
+        # padding if text data too short
+        padding = np.zeros((1, 100 - input_data.shape[1]), dtype=np.int32)
+        input_data = np.concatenate([input_data, padding], axis=1)
+    interpreter.set_tensor(input_details[0]['index'], input_data)
+    interpreter.invoke()
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+    result = output_data[0][0]
+
+    # return result
+    result = int(result > 0.5)
+    return JSONResponse(content=result)
+
+
+@app.on_event("startup")
+async def startup():
+    # Load the TFLite model
+    global interpreter
+    interpreter = tf.lite.Interpreter(model_path="./model/model.tflite")
+    interpreter.allocate_tensors()
+
+    # Download the IndoBERT tokenizer
+    global bert_tokenizer
+    bert_tokenizer = BertTokenizer.from_pretrained('cahya/bert-base-indonesian-522M')
+
+    # Get the input and output details
+    global input_details
+    global output_details
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
 
 if __name__ == "__main__":
-    uvicorn.run(app)
+    uvicorn.run(app, on_startup=startup)
